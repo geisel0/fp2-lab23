1f060103905a637b354c4f7bf91176ebff23b4f2
geisel0 <gpachecome@unsa.edu.pe>
2024-12-10 18:57:33 -0500

    añadido JPanel y botones

--- src/JuegoGUI.java
+++ src/JuegoGUI.java
@@ -1,3 +1,31 @@
-public class JuegoGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class JuegoGUI extends JFrame {
+    private Tablero tablero;
+    private Ejercito ejercito1;
+    private Ejercito ejercito2;
+    private JTextArea areaEstado;
+
+    public JuegoEstrategiaGUI() {
+        setTitle("Juego de Estrategia");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        tablero = new Tablero(10, 10);
+        ejercito1 = new Ejercito("Inglaterra", tablero, "bosque");
+        ejercito2 = new Ejercito("Francia", tablero, "campo abierto");
+
+        JPanel panelPrincipal = new JPanel(new BorderLayout());
+        JPanel panelControles = new JPanel(new GridLayout(3, 2));
+        areaEstado = new JTextArea(10, 50);
+        areaEstado.setEditable(false);
+        JScrollPane scrollEstado = new JScrollPane(areaEstado);
+
+        JButton btnMover = new JButton("Mover Soldado");
+        JButton btnMostrar = new JButton("Mostrar Tablero");
+    }
 }
